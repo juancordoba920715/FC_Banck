b2f262eb99bd4876e2749dfb0d050e58f5b73de2
juancordoba920715 <camilocr920715@gmail.com>
2023-10-21 23:05:34 -0500

    En proceso

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
-do {
-    
-} while (condition);
+let vista2 = document.getElementById('menu_transaccion')
+let formulario = document.getElementById('formulario')
+
+
+function login_in(){
+    vista2.style.display = "block"            
+    formulario.style.display ='none'
+}
+
+function Cierre_sesion(){
+    vista2.style.display = "none"    
+    formulario.style.display = "block";
+}   
+
+function consulta_saldo(){
+    let saldo = document.getElementById('consulta_saldo')
+    vista2.style.display ="none"
+    saldo.style.display = "block"  
+
+}
